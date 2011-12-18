c9ad335d36e4f9f7310e30493a0bf280a91758e3
Audrey Tang <audreyt@audreyt.org>
2011-12-18 06:22:16 -0800

    * Replace OSSP::uuid with Data::UUID (the latter is far easier to build.)

--- nlw/lib/Socialtext/UUID.pm
+++ nlw/lib/Socialtext/UUID.pm
@@ -2,7 +2,7 @@ package Socialtext::UUID;
 use warnings;
 use strict;
 use base 'Exporter';
-use OSSP::uuid ();
+use Data::UUID ();
 
 our @EXPORT    = qw(new_uuid);
 our @EXPORT_OK = qw(new_uuid);
@@ -10,9 +10,8 @@ our @EXPORT_OK = qw(new_uuid);
 my $uuid;
 
 sub new_uuid () {
-    $uuid ||= OSSP::uuid->new;
-    $uuid->make("v4");
-    return $uuid->export("str");
+    $uuid ||= Data::UUID->new;
+    return lc $uuid->create_str;
 }
 
 1;
